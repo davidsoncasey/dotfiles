65cf4d30fd7ef92a2631b42259cf6e6d1c9bd06e
Casey Davidson <cdavidson@workwithopal.com>
2020-04-02 10:31:00 -0700

    Add zprofile

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 cd ~
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" "" --unattended
 ln -sv ~/dotfiles/runcom/.bash_profile ~
+ln -sv ~/dotfiles/runcom/.zprofile ~
